343f32d5b640eef40ce79d06cbb87d828fc426c2
Matt Glover <matthew.glover@peeq-digital.com>
2013-11-21 10:26:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneLeaflet::Application.config.secret_key_base = '45d970905ba821fba5eac761a98a63b880f2fb520f344670488f4f4753795237ae6f096b8dc3e229fe0b271b48f1ff045e9899cea8897e3495eb666733ff04a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneLeaflet::Application.config.session_store :cookie_store, key: '_backbone_leaflet_session'
